45f84378f26ca883d7147e4712973231d3053dee
Marco X <mex_marcox@outlook.es>
2024-09-24 20:57:14 -0600

    commit second

---
+++ matebasica.py
@@ -0,0 +1,4 @@
+# Tipos de datos
+print(type('esto es texto'))
+print(type(4))
+print(type(1.5))
---
+++ variable.py
@@ -0,0 +1,15 @@
+#variables
+  
+a = 4
+b = 6
+
+c = a + b
+print (c)
+
+mi_numero = 29
+ 
+mi_texto ='texto'
+
+print(type(mi_texto))
+print (mi_texto,mi_numero)
+print('mi numero favorito es:',mi_numero)
